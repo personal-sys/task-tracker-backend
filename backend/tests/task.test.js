a2175db0813f428cd337f203510000d576c1adff
Durga <durgachandana0309@gmail.com>
2026-01-18 17:08:43 +0530

    Added test cases

---
+++ backend/tests/task.test.js
@@ -0,0 +1,11 @@
+// backend/tests/task.test.js
+const request = require("supertest");
+const app = require("../server");
+
+test("POST /tasks creates task", async () => {
+  const res = await request(app)
+    .post("/tasks")
+    .send({ title: "Learn Git" });
+
+  expect(res.statusCode).toBe(201);
+});
