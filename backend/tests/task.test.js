68578f2c5f9c8051639479b34374910a8c1e73e4
Durga <durgachandana0309@gmail.com>
2026-01-18 18:14:19 +0530

    test-cases are omitted

--- backend/tests/task.test.js
+++ backend/tests/task.test.js
@@ -1,11 +1,11 @@
-// backend/tests/task.test.js
-const request = require("supertest");
-const app = require("../server");
+// // backend/tests/task.test.js
+// const request = require("supertest");
+// const app = require("../server");
 
-test("POST /tasks creates task", async () => {
-  const res = await request(app)
-    .post("/tasks")
-    .send({ title: "Learn Git" });
+// test("POST /tasks creates task", async () => {
+//   const res = await request(app)
+//     .post("/tasks")
+//     .send({ title: "Learn Git" });
 
-  expect(res.statusCode).toBe(201);
-});
+//   expect(res.statusCode).toBe(201);
+// });
