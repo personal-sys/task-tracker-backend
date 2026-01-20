112b74fb1cfcba3026af66b81fc83d9e932e49fe
Durga <durgachandana0309@gmail.com>
2026-01-20 15:11:33 +0530

    code changed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 /* -------------------- MongoDB Connection -------------------- */
 async function connectDB() {
   await mongoose.connect(process.env.MONGO_URL);
-  console.log("MongoDB Connection succesful and server running");
+  console.log(" succesful and server running");
 }
 
 connectDB().catch(err => console.log("MongoDB Error:", err));
